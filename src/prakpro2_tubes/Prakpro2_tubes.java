f32df25541033653357c92f6f9837244ec044782
Syahnan <syhnnzhra@gmail.com>
2023-12-18 07:07:12 +0700

    tubes prakpro2

---
+++ src/prakpro2_tubes/Prakpro2_tubes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prakpro2_tubes;
+
+/**
+ *
+ * @author syhnn
+ */
+public class Prakpro2_tubes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
